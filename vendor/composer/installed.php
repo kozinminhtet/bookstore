9e7755ccedb9ff73acc8f7e38cc85347cae5c80c
Zin Min Htet <zinminhtet.2016.mr@gmail.com>
2024-08-21 23:29:42 +0630

    aaaa

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e550489ec263d73bb803eb21bfdcd8efae8b3cef',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => true,
+        'dev' => false,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e550489ec263d73bb803eb21bfdcd8efae8b3cef',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
